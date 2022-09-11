3c8572b5761e3dc4eb92ee2cd02467e9a089a3f3
ZXCMONSTER <wertunwertun@rambler.ru>
2022-09-11 16:05:29 +0300

    MEDIA_SOLVE

--- Bclub/urls.py
+++ Bclub/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import *
@@ -28,5 +29,6 @@ urlpatterns = [
     path('registationotlogin/',Registr.as_view(),name='logorreg'),
     path('users/', AllUsers.as_view(),name='users'),
     path('user/',include('User.urls')),
+    re_path(r'^media/(?P<path>.*)$',serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns() 
